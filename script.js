37b4405ed1c8b8ce7420a01c51e93e30457f0c4c
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-02-21 22:46:07 +0200

    all calculate is complete

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const swap = document.getElementById('swap');
 
 const API_URL = 'https://api.freecurrencyapi.com/v1/latest?apikey=J6e4atuaW0NwEQIkh1eMkOAGzdwc1pkZDsDFha5W'
 let html= '';
+let html_two ='';
 
 
 async function currency(url){
@@ -30,13 +31,18 @@ async function renderCurrency(data){
         return html += `<option value="${item}" placeholder="0">${item}</option>`;
     })
     currencyEl_one.innerHTML = html;
-    currencyEl_two.innerHTML = html;
-    console.log(currencyEl_one)
-    console.log(currencyEl_one.value);
-    console.log(data['TRY'])
-    return calculate(data);
 
- function calculate(data){
+    [arrKeys[0],arrKeys[8]]= [arrKeys[8],arrKeys[0]];
+    arrKeys.map(item=>{
+         return html_two +=`<option value="${item}" placeholder="0">${item}</option>`;
+    });
+    currencyEl_two.innerHTML = html_two;
+    amountEl_two.value = amountEl_one.value * data[currencyEl_two.value] / data[currencyEl_one.value];
+    return calculate(data)
+}
+
+    function calculate(data){
+
         amountEl_one.addEventListener('keyup', ()=>{
             amountEl_two.value = amountEl_one.value * data[currencyEl_two.value] / data[currencyEl_one.value];
         });
@@ -46,28 +52,28 @@ async function renderCurrency(data){
          currencyEl_one.addEventListener('change', ()=>{
         amountEl_two.value = amountEl_one.value * data[currencyEl_two.value] / data[currencyEl_one.value];
         });
-        currencyEl_one.addEventListener('change', ()=>{
+        currencyEl_two.addEventListener('change', ()=>{
+            if(amountEl_two.value !== null){
+                amountEl_two.value = data[currencyEl_two.value] / data[currencyEl_one.value];;
+            }
             amountEl_one.value = amountEl_two.value * data[currencyEl_one.value] / data[currencyEl_two.value];
         });
         swap.addEventListener('click', ()=>{
-            let temp = currencyEl_one.value;
-            console.log(currencyEl_one)
+            const temp = currencyEl_one.value;
             currencyEl_one.value=currencyEl_two.value;
-            currencyEl_two=temp;
-            calculate(data);
+            currencyEl_two.value=temp;
+            amountEl_two.value = (amountEl_one.value * data[currencyEl_two.value])/ data[currencyEl_one.value];
+            amountEl_one.value = (amountEl_two.value * data[currencyEl_one.value]) / data[currencyEl_two.value];
+           
         })
     }
-
-}
-
-   
- 
+    
 
 async function main() {
 
     try{
      const data= await currency(API_URL);
-     const currencyDeploy = await renderCurrency(data);
+     const currencyDeploy = await renderCurrency(data)
       
     }catch(error){
       console.log(error)
