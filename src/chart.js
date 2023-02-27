94e81ed30dd543705d1f56939b25d52c71aee425
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-02-27 13:48:17 +0200

    proje complete

--- src/chart.js
+++ src/chart.js
@@ -1,109 +1,101 @@
-
 export const today = new Date();
-export const endDay = String(today.getDate()-1).padStart(2, '0');
-export const startDay = String(today.getDate()-16).padStart(2, '0');
-export const mont = String(today.getMonth() + 1).padStart(2, '0');
+export const endDay = String(today.getDate() - 1).padStart(2, "0");
+export const startDay = String(today.getDate() - 16).padStart(2, "0");
+export const mont = String(today.getMonth() + 1).padStart(2, "0");
 export const year = today.getFullYear();
 
-export const dateTo = year + '-' + mont + '-' + endDay;
-export const dateFrom = year + '-' + mont + '-' + startDay;
+export const dateTo = year + "-" + mont + "-" + endDay;
+export const dateFrom = year + "-" + mont + "-" + startDay;
 
-const ctx = document.getElementById('currency-chart-one');
-const ctx2 = document.getElementById('currency-chart-two');
-
+const ctx = document.getElementById("currency-chart-one");
+const ctx2 = document.getElementById("currency-chart-two");
 
 let myChart;
 let myChart2;
 
-  export const chart =  async (url,valueOne,valueTwo) =>{
-    try{
-    const res = await fetch(url);
-    if(!res.ok){
-      throw new Error(`HTTP error!`);
-    } 
-    const dateHistory = await res.json();
-    const { data } = dateHistory;
+export const chart = async (url, valueOne, valueTwo) => {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`HTTP error!`);
+  }
+  const dateHistory = await res.json();
+  const { data } = dateHistory;
 
-    const labelDate = Object.keys(data);
-  
-    if (myChart) {
-      myChart.destroy();
-    }
+  const labelDate = Object.keys(data);
 
-    if (myChart2) {
-      myChart2.destroy();
-    }
-  
-    const valueData = [];
-    for (const date in data) {
-      if (data.hasOwnProperty(date)) {
-        valueData.push(data[date][valueOne.value]);
-      }
-    }
+  if (myChart) {
+    myChart.destroy();
+  }
 
-    const valueData2 = []; 
-    for (const date in data) {
-      if (data.hasOwnProperty(date)) {
-        valueData2.push(data[date][valueTwo.value]);
-      }
-    }
-  
-     myChart= new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: labelDate,
-        datasets: [
-          {
-            label: `${valueOne.value}/USD`,
-            backgroundColor:'#ffb6c1',
-            borderColor:'#f08080',
-            data: valueData,
-            tension:0.4,
-            yAxisID: 'percentage'
-          },
-        ],
-      },
-  
-      options: {
-        scales: {
-          x: {
-            ticks : {
-              maxTicksLimit: 3,
-            }
-          }
-        },
-      },
-    });
-    
-    myChart2 = new Chart(ctx2, {
-      type: 'line',
-      data: {
-        labels: labelDate,
-        datasets: [
-          {
-            label: `${valueTwo.value}/USD`,
-            backgroundColor:'#f0f8ff',
-            borderColor:'#00ced1',
-            data: valueData2,
-            tension: 0.4,
-            yAxisID:'percentage',
-          },
-        ],
-      },
-  
-      options: {
-        scales: {
-          x: {
-            ticks : {
-              maxTicksLimit: 3,
-            }
-          }
-        },
-      },
-    });
-    }catch{
-      console.log(error);
-      alert('Unable to load charts')
+  if (myChart2) {
+    myChart2.destroy();
+  }
+
+  const valueData = [];
+  for (const date in data) {
+    if (data.hasOwnProperty(date)) {
+      valueData.push(data[date][valueOne.value]);
     }
   }
-   
+
+  const valueData2 = [];
+  for (const date in data) {
+    if (data.hasOwnProperty(date)) {
+      valueData2.push(data[date][valueTwo.value]);
+    }
+  }
+
+  myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labelDate,
+      datasets: [
+        {
+          label: `${valueOne.value}/USD`,
+          backgroundColor: "#ffb6c1",
+          borderColor: "#f08080",
+          data: valueData,
+          tension: 0.4,
+          yAxisID: "percentage",
+        },
+      ],
+    },
+
+    options: {
+      scales: {
+        x: {
+          ticks: {
+            maxTicksLimit: 3,
+          },
+        },
+      },
+    },
+  });
+
+  myChart2 = new Chart(ctx2, {
+    type: "line",
+    data: {
+      labels: labelDate,
+      datasets: [
+        {
+          label: `${valueTwo.value}/USD`,
+          backgroundColor: "#f0f8ff",
+          borderColor: "#00ced1",
+          data: valueData2,
+          tension: 0.4,
+          yAxisID: "percentage",
+        },
+      ],
+    },
+
+    options: {
+      scales: {
+        x: {
+          ticks: {
+            maxTicksLimit: 3,
+          },
+        },
+      },
+    },
+  });
+};
--- src/script.js
+++ src/script.js
@@ -3,96 +3,115 @@ import { dateFrom } from "./chart.js";
 import { dateTo } from "./chart.js";
 import { currencySymbols } from "./symbol.js";
 
+const currencyEl_one = document.getElementById("currency-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_one = document.getElementById("amount-one");
+const amountEl_two = document.getElementById("amount-two");
 
-const currencyEl_one= document.getElementById('currency-one');
-const currencyEl_two= document.getElementById('currency-two');
-const amountEl_one= document.getElementById('amount-one');
-const amountEl_two= document.getElementById('amount-two');
+const swap = document.getElementById("swap");
 
-const swap = document.getElementById('swap');
+const API_URL =
+  "https://api.freecurrencyapi.com/v1/latest?apikey=J6e4atuaW0NwEQIkh1eMkOAGzdwc1pkZDsDFha5W";
+const url_chart = `https://api.freecurrencyapi.com/v1/historical?apikey=J6e4atuaW0NwEQIkh1eMkOAGzdwc1pkZDsDFha5W&date_from=${dateFrom}&date_to=${dateTo}`;
 
-const API_URL = 'https://api.freecurrencyapi.com/v1/latest?apikey=J6e4atuaW0NwEQIkh1eMkOAGzdwc1pkZDsDFha5W'
-const url_chart =`https://api.freecurrencyapi.com/v1/historical?apikey=J6e4atuaW0NwEQIkh1eMkOAGzdwc1pkZDsDFha5W&date_from=${dateFrom}&date_to=${dateTo}`
+let html = "";
+let htmlTwo = "";
 
-let html= '';
-let htmlTwo ='';
+chart(url_chart, currencyEl_one, currencyEl_two).catch((error) => {
+  alert(`chart cant load:${error.message}`);
+});
 
-chart(url_chart,currencyEl_one,currencyEl_two);
-
-async function currency(url){
-    const res = await fetch(url);
-    if(!res.ok){
-        throw new Error(`HTTP error!`);
-    }
-    const dataCurrency = await res.json()
-    const{data} = dataCurrency;
-    return data
+async function currency(url) {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(`HTTP error!`);
+  }
+  const dataCurrency = await res.json();
+  const { data } = dataCurrency;
+  return data;
 }
 
-async function renderCurrency(data){
-  
-    const arrKeys = Object.keys(data);
+async function renderCurrency(data) {
+  const arrKeys = Object.keys(data);
 
-    [arrKeys[0],arrKeys[30]]= [arrKeys[30],arrKeys[0]];  /*I choose USD as initial element */
-    arrKeys.map(item=>{
-        return html += `<option value="${item}" >${currencySymbols[item]} ${item}</option>`;
-    })
-    currencyEl_one.innerHTML = html;
+  [arrKeys[0], arrKeys[30]] = [
+    arrKeys[30],
+    arrKeys[0],
+  ]; /*I choose USD as initial element */
+  arrKeys.map((item) => {
+    return (html += `<option value="${item}" >${currencySymbols[item]} ${item}</option>`);
+  });
+  currencyEl_one.innerHTML = html;
 
-    [arrKeys[0],arrKeys[8]]= [arrKeys[8],arrKeys[0]];   /*I choose EURO as initial element */
-    arrKeys.map(item=>{
-         return htmlTwo +=`<option value="${item}" >${currencySymbols[item]} ${item}</option>`;
-    });
-    currencyEl_two.innerHTML = htmlTwo;
+  [arrKeys[0], arrKeys[8]] = [
+    arrKeys[8],
+    arrKeys[0],
+  ]; /*I choose EURO as initial element */
+  arrKeys.map((item) => {
+    return (htmlTwo += `<option value="${item}" >${currencySymbols[item]} ${item}</option>`);
+  });
+  currencyEl_two.innerHTML = htmlTwo;
 
-    amountEl_two.value = (amountEl_one.value * data[currencyEl_two.value] / data[currencyEl_one.value]).toFixed(5);
-    return data
-
+  amountEl_two.value = (
+    (amountEl_one.value * data[currencyEl_two.value]) /
+    data[currencyEl_one.value]
+  ).toFixed(5);
+  return data;
 }
 
-    function calculate(data){
-        
-        amountEl_one.addEventListener('keyup', ()=>{
-            amountEl_two.value = (amountEl_one.value * data[currencyEl_two.value] / data[currencyEl_one.value]).toFixed(5);
-            chart(url_chart,currencyEl_one,currencyEl_two);
-        });
-        
-        amountEl_two.addEventListener('keyup', ()=>{
-            amountEl_one.value = (amountEl_two.value * data[currencyEl_one.value] / data[currencyEl_two.value]).toFixed(5);
-            chart(url_chart,currencyEl_one,currencyEl_two);
-        });
+function calculate(data) {
+  amountEl_one.addEventListener("keyup", () => {
+    amountEl_two.value = (
+      (amountEl_one.value * data[currencyEl_two.value]) /
+      data[currencyEl_one.value]
+    ).toFixed(5);
+    chart(url_chart, currencyEl_one, currencyEl_two);
+  });
 
-        currencyEl_one.addEventListener('change', ()=>{
-            amountEl_two.value = (amountEl_one.value * data[currencyEl_two.value] / data[currencyEl_one.value]).toFixed(5);
-            chart(url_chart,currencyEl_one,currencyEl_two);
-        });
+  amountEl_two.addEventListener("keyup", () => {
+    amountEl_one.value = (
+      (amountEl_two.value * data[currencyEl_one.value]) /
+      data[currencyEl_two.value]
+    ).toFixed(5);
+    chart(url_chart, currencyEl_one, currencyEl_two);
+  });
 
-        currencyEl_two.addEventListener('change', ()=>{
-            amountEl_one.value = (amountEl_two.value * data[currencyEl_one.value] / data[currencyEl_two.value]).toFixed(5);
-            chart(url_chart,currencyEl_one,currencyEl_two);
-        });
+  currencyEl_one.addEventListener("change", () => {
+    amountEl_two.value = (
+      (amountEl_one.value * data[currencyEl_two.value]) /
+      data[currencyEl_one.value]
+    ).toFixed(5);
+    chart(url_chart, currencyEl_one, currencyEl_two);
+  });
 
-        swap.addEventListener('click', ()=>{
-            const temp = currencyEl_one.value;
-            currencyEl_one.value=currencyEl_two.value;
-            currencyEl_two.value=temp;
-            amountEl_two.value = ((amountEl_one.value * data[currencyEl_two.value])/ data[currencyEl_one.value]).toFixed(5);
-            
-        })
-    }
-    
+  currencyEl_two.addEventListener("change", () => {
+    amountEl_one.value = (
+      (amountEl_two.value * data[currencyEl_one.value]) /
+      data[currencyEl_two.value]
+    ).toFixed(5);
+    chart(url_chart, currencyEl_one, currencyEl_two);
+  });
+
+  swap.addEventListener("click", () => {
+    const temp = currencyEl_one.value;
+    currencyEl_one.value = currencyEl_two.value;
+    currencyEl_two.value = temp;
+    amountEl_two.value = (
+      (amountEl_one.value * data[currencyEl_two.value]) /
+      data[currencyEl_one.value]
+    ).toFixed(5);
+  });
+}
 
 async function main() {
-
-    try{
-     const data= await currency(API_URL);
-     const currencyDeploy = await renderCurrency(data);
-     const CalculatePart =  calculate(data);
-    }catch(error){
-      console.error(error)
-      alert('Something wrong');
-    }
+  try {
+    const data = await currency(API_URL);
+    await renderCurrency(data);
+    calculate(data);
+  } catch (error) {
+    console.error(error);
+    alert("Something went wrong");
   }
+}
 
-window.addEventListener('load', main);
-
+window.addEventListener("load", main);
