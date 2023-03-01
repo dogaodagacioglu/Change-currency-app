5603c8c6806618ba9342e916a58e5c3481d75780
Doğa ödağacıoğlu <agarwoodson@icloud.com>
2023-03-01 15:31:49 +0200

    ı try to fixed date

--- src/chart.js
+++ src/chart.js
@@ -1,11 +1,25 @@
-export const today = new Date();
-export const endDay = String(today.getDate() - 1).padStart(2, "0");
-export const startDay = String(today.getDate() - 16).padStart(2, "0");
-export const mont = String(today.getMonth() + 1).padStart(2, "0");
-export const year = today.getFullYear();
 
-export const dateTo = year + "-" + mont + "-" + endDay;
-export const dateFrom = year + "-" + mont + "-" + startDay;
+
+const today = new Date();
+let endDay = String(today.getDate() - 1).padStart(2, "0");
+let endMonth = String(today.getMonth() + 1).padStart(2, "0");
+let startDay = String(today.getDate() - 16).padStart(2, "0");
+let startMonth = String(today.getMonth() + 1).padStart(2, "0");
+const year = today.getFullYear();
+
+if (today.getDate() <= 16) {
+   startDay = String(today.getDate() + 14).padStart(2, "0");
+   startMonth = String(today.getMonth()).padStart(2, "0");
+}
+
+if (today.getDate() === 1) {
+   const lastDayOfLastMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+   endDay = String(lastDayOfLastMonth.getDate()).padStart(2, "0");
+   endMonth = String(lastDayOfLastMonth.getMonth() + 1).padStart(2, "0");
+}
+
+export const dateTo = year + "-" + endMonth + "-" + endDay;
+export const dateFrom = year + "-" + startMonth + "-" + startDay;
 
 const ctx = document.getElementById("currency-chart-one");
 const ctx2 = document.getElementById("currency-chart-two");
